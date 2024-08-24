8cd539c41c9af83580140ce77b6207dd46415bf2
Amit Jana <101209091+amit-954@users.noreply.github.com>
2024-08-24 20:26:33 +0530

    Update CustomerLifetimeValueChart.jsx

--- client/src/components/CustomerLifetimeValueChart.jsx
+++ client/src/components/CustomerLifetimeValueChart.jsx
@@ -38,7 +38,7 @@ const CustomerLifetimeValueCohortsChart = () => {
 		try {
 			setLoading(true);
 			const response = await axios.get(
-				`http://localhost:5000/api/analytics/customer-lifetime-value`,
+				`https://analytics-server-kklo.onrender.com/api/analytics/customer-lifetime-value`,
 			);
 			console.log("Response received:", response.data);
 
